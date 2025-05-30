e71e7b9d57ebcd24b8a14d87b392c03b61d41e59
Jhonatan Sampaio <jhonatan.sampaiof@gmail.com>
2025-05-29 21:52:01 -0300

    First commit

---
+++ src/main/java/fiap/kciao/gestao_solidaria/GestaoSolidarIaApplication.java
@@ -0,0 +1,13 @@
+package fiap.kciao.gestao_solidaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoSolidarIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoSolidarIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/kciao/gestao_solidaria/GestaoSolidarIaApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.kciao.gestao_solidaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoSolidarIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
